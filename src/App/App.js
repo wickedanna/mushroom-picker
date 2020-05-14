f1035a9156e9116b47e20c53bf319f7e47e21966
Kayla Melton <kannmelton2@gmail.com>
2020-05-13 20:36:33 -0500

    corrected some data, set state and returning data properly

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import './App.scss';
 
+import mushroomData from '../helpers/data/mushroomData';
+
 class App extends React.Component {
+  state = {
+    mushrooms: [],
+  }
+
+  componentDidMount() {
+    const mushrooms = mushroomData.getMushrooms();
+    this.setState({ mushrooms });
+  }
+
   render() {
     return (
       <div className="App">
